90269e7efbbe47e9f87370c31524573d2d8ecacd
mayara.cardoso <mayara.cardoso@sances.com.br>
2025-03-19 21:37:06 -0300

    regras

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// simular o processo, mostrar os consoles, o que acontece com cada processo ao decorrer do tempo
 // simular que tal processo está acessando recurso x e coloca na fila
+
+// quando o coordenador morrer ele precisa avisar e gerar um random dos processos que existem
+// os processos estarao numa lista de objetos que pertence ao coordenador 
+// a lista deve contem o ID do processo e a data  hora que foi criado (eu acho)
+// o coordenador pode ser de acesso global 
+// o processo tenta a cada 10-25 segundos o recurso, so é adicionado na fila se ainda nao tiver
